a7e224b00413c839e226087ffffe7c1b216139d9
Ejaz-100400 <ejazahmed4688@gmail.com>
2022-11-17 10:15:15 +0530

    replacing card-description

--- src/Components/Cards.jsx
+++ src/Components/Cards.jsx
@@ -5,6 +5,7 @@ export default function Cards(props){
         <div className="card">
             <img className="card-img"src={props.cardimg} width="353" height="415"></img>
             <div className="card--context">
+            <h3 className="card--context--desc text-light w-50">{props.cardname}</h3>
             <button className="btn btn-dark w-75">LEARN MORE <span className="font-weight-bold">+</span></button>
             </div>
         </div>
