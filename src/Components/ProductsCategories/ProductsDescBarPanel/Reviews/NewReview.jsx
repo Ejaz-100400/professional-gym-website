9682e10f33741ac0706a802c55f40ec0f379524a
Ejaz-100400 <ejazahmed4688@gmail.com>
2022-11-20 12:52:53 +0530

    20/11/2022 afternoon

--- src/Components/ProductsCategories/ProductsDescBarPanel/Reviews/NewReview.jsx
+++ src/Components/ProductsCategories/ProductsDescBarPanel/Reviews/NewReview.jsx
@@ -1,25 +1,36 @@
 import React from "react"
 export default function NewReview(){
-    const[star,setstar]=React.useState(false)
+    const[rating,setrating]=React.useState(null)
+    const[hoverrating,sethoverrating]=React.useState(null)
     const [closeform,setcloseform]=React.useState(false)
     function closebtnform(){
         setcloseform(prevState=>!prevState)
     } 
     return(
-        <div className="products-newreview" style={{display:closeform?'block':'none'}}>
+        <div className="products-newreview" style={{display:closeform?'block':'none',color:'white'}}>
         <div className="products--newreview--panel" id='newreview'>
-                <i onClick={closebtnform}class="fa-solid fa-xmark position-absolute fa-2x"></i>
+                <i onClick={closebtnform}class="fa-solid fa-xmark position-absolute fa-2x text-danger"></i>
                 <h4 className="text-center">WRITE REVIEW, PUBLISH</h4>
                 <hr></hr>
-                <form className="form--review d-grid p-2">
+                <form className="form--review d-grid p-2 m-1">
                     <div className="input-group d-flex">
                     <h5>Rating</h5>
                     <div className="star-rating d-flex">
-                    <i className="fa-regular fa-star"></i>
-                    <i className="fa-regular fa-star"></i>
-                    <i className="fa-regular fa-star"></i>
-                    <i className="fa-regular fa-star"></i>
-                    <i className="fa-regular fa-star"></i>
+                        {[...Array(5)].map((star,i) =>{
+                            const ratingval=i+1
+                            return (
+                            <label>
+                                <input type='radio' 
+                                name='rating' 
+                                value={ratingval}
+                                onClick={()=>setrating(ratingval)} />
+                                <i className="fa-solid fa-star fa-1x"
+                                onMouseEnter={()=>sethoverrating(ratingval)}
+                                onMouseLeave={()=>sethoverrating( null )}
+                                style={{color:ratingval<=(hoverrating ||rating)?'yellow':'silver'}}></i>
+                            </label>
+                            )
+                        })}
                     </div>
                     </div>
                     <div className="input-group p-2 d-grid">
